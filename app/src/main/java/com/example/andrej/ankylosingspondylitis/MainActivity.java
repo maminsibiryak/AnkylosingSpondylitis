d19794a82ccd4aaf477d7cc9b8f79a9303022ca4
andrej <iandrewka@gmail.com>
2018-05-01 10:52:02 +0800

    about_the_desease
    Добавил первый пунк меню - краткое описание болезни

--- app/src/main/java/com/example/andrej/ankylosingspondylitis/MainActivity.java
+++ app/src/main/java/com/example/andrej/ankylosingspondylitis/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.andrej.ankylosingspondylitis;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.andrej.ankylosingspondylitis.anotheractivity.AboutTheDiseaseActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button aboutTheDisease;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        aboutTheDisease = findViewById(R.id.aboutTheDiseaseButton);
+    }
+
+    public void goToTheDescriptionOfTheDisease (View view){
+        Intent intent = new Intent(this, AboutTheDiseaseActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/andrej/ankylosingspondylitis/anotheractivity/AboutTheDiseaseActivity.java
@@ -0,0 +1,15 @@
+package com.example.andrej.ankylosingspondylitis.anotheractivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.andrej.ankylosingspondylitis.R;
+
+public class AboutTheDiseaseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_the_disease);
+    }
+}
