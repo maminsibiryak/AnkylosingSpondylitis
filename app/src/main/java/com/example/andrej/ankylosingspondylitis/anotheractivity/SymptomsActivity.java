fa669fc7dd6f48ce4fe679b01eb58faa3bd2931f
andrej <iandrewka@gmail.com>
2018-05-06 10:53:49 +0800

    about_the_symptoms
    new activity and xml about symptoms

---
+++ app/src/main/java/com/example/andrej/ankylosingspondylitis/anotheractivity/SymptomsActivity.java
@@ -0,0 +1,15 @@
+package com.example.andrej.ankylosingspondylitis.anotheractivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.andrej.ankylosingspondylitis.R;
+
+public class SymptomsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_symptoms);
+    }
+}
