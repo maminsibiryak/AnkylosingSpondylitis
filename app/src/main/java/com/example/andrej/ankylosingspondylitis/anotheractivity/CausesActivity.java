b5dbceb7525ce6e9a39604741ad7879f6d56fb24
andrej <iandrewka@gmail.com>
2018-05-08 18:21:52 +0800

    causes_activity
    add new activity

---
+++ app/src/main/java/com/example/andrej/ankylosingspondylitis/anotheractivity/CausesActivity.java
@@ -0,0 +1,22 @@
+package com.example.andrej.ankylosingspondylitis.anotheractivity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Html;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
+
+import com.example.andrej.ankylosingspondylitis.R;
+
+public class CausesActivity extends AppCompatActivity {
+
+    private TextView causesTextview;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_causes);
+        causesTextview = findViewById(R.id.causesTextView);
+        causesTextview.setMovementMethod(new ScrollingMovementMethod());
+        causesTextview.setText(Html.fromHtml(getString(R.string.title_activity_causes)));
+    }
+}
